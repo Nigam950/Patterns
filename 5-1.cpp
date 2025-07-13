fd582946be2b90f1515952eac156cbbaf3ff0d75
jay_nigam <Nigamjay57@gmail.com>
2025-07-13 19:38:53 +0530

    Char and star

---
+++ 5-1.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+void pattern(int n)
+{
+  for(int i=1;i<=n;i++)
+  {
+    for(int j=0;j<=n-i;j++)
+    {
+      cout<<" ";
+    }
+    for(int k=1;k<=i;k++)
+    {
+        cout<<k<<" ";
+    }
+    cout<<endl;
+  }
+}
+int main()
+{
+    int a;
+    cout<<"Enter the number of rows:";
+    cin>>a;
+    pattern(a);
+    return 0;
+}
