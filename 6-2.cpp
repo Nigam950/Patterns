52ad873d9247f4932be3b0898d088ddaa0ff77fb
jay_nigam <Nigamjay57@gmail.com>
2025-07-14 10:47:41 +0530

    inverted pyramid

---
+++ 6-2.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+void pattern(int n)
+{
+  for(int i=n;i>0;i--)
+  {
+    for(int j=0;j<=n-i;j++)
+    {
+      cout<<" ";
+    }
+    for(int k=0;k<i;k++)
+    {
+        cout<<char('A'+ k)<<" ";
+    }
+    cout<<endl;
+  }
+}
+int main()
+{
+    int a;
+    cout<<"Enter the number of rows:";
+    cin>>a;
+    pattern(a);
+    return 0;
+}
---
+++ 6.cpp
@@ -0,0 +1,26 @@
+// inverted pyramid
+#include<iostream>
+using namespace std;
+void pattern(int n)
+{
+  for(int i=n;i>0;i--)
+  {
+    for(int j=0;j<n-i;j++)
+    {
+      cout<<" ";
+    }
+    for(int k=1;k<=i;k++)
+    {
+        cout<<"* ";
+    }
+    cout<<endl;
+  }
+}
+int main()
+{
+    int a;
+    cout<<"Enter the number of rows:";
+    cin>>a;
+    pattern(a);
+    return 0;
+}
