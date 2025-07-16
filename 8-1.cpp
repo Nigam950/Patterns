eb473be1e553c6b58cd0d70d0799f64e4f166a88
jay_nigam <Nigamjay57@gmail.com>
2025-07-16 11:23:35 +0530

    Diamond

---
+++ 8-1.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+void pattern(int n)
+{
+  for(int i=0;i<n-1;i++)
+  {
+    for(int j=0;j<n-i;j++)
+    {
+      cout<<" ";
+    }
+    for(int k=0;k<=i;k++)
+    {
+        cout<<k<<" ";
+    }
+    cout<<endl;
+  }
+    for(int i=n;i>0;i--)
+  {
+    for(int j=-1;j<n-i;j++)
+    {
+      cout<<" ";
+    }
+    for(int k=1;k<=i;k++)
+    {
+        cout<<k<<" ";
+    }
+    cout<<endl;
+  }
+}
+int main()
+{
+    int a;
+    cout<<"Enter the number of rows:";
+    cin>>a;
+    pattern(a);
+    return 0;
+}
