c1fe8e04e3512c1e51911c4b88deec6feef9782e
jay_nigam <Nigamjay57@gmail.com>
2025-07-17 19:35:48 +0530

    Glass Hour

---
+++ 9.cpp
@@ -0,0 +1,39 @@
+// glass hour
+#include<iostream>
+using namespace std;
+void pattern(int n)
+{
+    for(int i=n;i>1;i--)
+  {
+    for(int j=-1;j<n-i;j++)
+    {
+      cout<<" ";
+    }
+    for(int k=1;k<=i;k++)
+    {
+        cout<<"* ";
+    }
+    cout<<endl;
+  }
+
+  for(int i=0;i<n;i++)
+  {
+    for(int j=0;j<n-i;j++)
+    {
+      cout<<" ";
+    }
+    for(int k=0;k<=i;k++)
+    {
+        cout<<"* ";
+    }
+    cout<<endl;
+  }
+}
+int main()
+{
+    int a;
+    cout<<"Enter the number of rows:";
+    cin>>a;
+    pattern(a);
+    return 0;
+}
