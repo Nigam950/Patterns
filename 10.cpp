f759df99de611763c3fdb1514e3fe0b1d1137f6b
jay_nigam <Nigamjay57@gmail.com>
2025-07-18 10:22:03 +0530

    Hollow Square

---
+++ 10.cpp
@@ -0,0 +1,30 @@
+// hollow square
+#include<iostream>
+using namespace std;
+void pattern(int n)
+{
+    for(int i=1; i<=n;i++)
+     {
+        for(int j=1;j<=n;j++)
+        {
+
+        
+        if(i==1 || i==n || j==1 || j==n )
+        {   
+            cout<<"* ";
+        }
+        else{
+           cout<<" "<<" ";
+        }
+    }
+        cout<<endl;
+     }
+}
+int main()
+{
+     int a;
+     cout<<"Enter the number of the rows:";
+     cin >>a;
+     pattern(a);
+    return 0;
+}
