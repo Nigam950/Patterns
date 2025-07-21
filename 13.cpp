b1fd622c34f5adebd6798c291387362ca5b53b36
jay_nigam <Nigamjay57@gmail.com>
2025-07-21 20:44:45 +0530

    patterns

---
+++ 13.cpp
@@ -0,0 +1,52 @@
+// hollow hourglass
+#include<iostream>
+using namespace std;
+void pattern(int n) 
+{
+   for(int i=n;i>=2;i--)
+   {
+    for(int j=0;j<n-i;j++)
+    {
+      cout<<" ";
+    }
+    for(int k=0;k<i;k++)
+    {
+      if(k==0|| i==n|| k==i-1)
+      {
+       cout<<"* ";
+      }
+      else
+      {
+        cout<<"  ";
+      }
+    }
+    cout<<endl;
+   }
+   for(int i=1;i<=n;i++)
+   {
+    for(int j=0;j<n-i;j++)
+    {
+      cout<<" ";
+    }
+    for(int k=0;k<i;k++)
+    {
+      if(k==0|| i==n|| k==i-1)
+      {
+       cout<<"* ";
+      }
+      else
+      {
+        cout<<"  ";
+      }
+    }
+    cout<<endl;
+   }
+}
+int main()
+{
+  int a;
+  cout<<"Enter the number of the rows:";
+  cin>>a;
+  pattern(a);
+  return 0;
+}
---
+++ 14.cpp
@@ -0,0 +1,52 @@
+// hollow diamond
+#include<iostream>
+using namespace std;
+void pattern(int n) 
+{
+   for(int i=0;i<=n-1;i++)
+   {
+    for(int j=0;j<n-i;j++)
+    {
+      cout<<" ";
+    }
+    for(int k=0;k<i;k++)
+    {
+      if(k==0|| k==i-1)
+      {
+       cout<<"* ";
+      }
+      else
+      {
+        cout<<"  ";
+      }
+    }
+    cout<<endl;
+   }
+   for(int i=n;i>=1;i--)
+   {
+    for(int j=0;j<n-i;j++)
+    {
+      cout<<" ";
+    }
+    for(int k=0;k<i;k++)
+    {
+      if(k==0|| k==i-1)
+      {
+       cout<<"* ";
+      }
+      else
+      {
+        cout<<"  ";
+      }
+    }
+    cout<<endl;
+   }
+}
+int main()
+{
+  int a;
+  cout<<"Enter the number of the rows:";
+  cin>>a;
+  pattern(a);
+  return 0;
+}
