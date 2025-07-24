b1a7f5603a8c4b2dec531abc3bc023e74da58165
jay_nigam <Nigamjay57@gmail.com>
2025-07-24 10:24:16 +0530

    Pascal's Triangle

---
+++ 16.cpp
@@ -0,0 +1,25 @@
+// pascal's triangle.
+#include<iostream>
+#include<cmath>
+using namespace std;
+void pattern(int n)
+{
+    for(int i=1;i<=n;i++)
+  {
+      int c=1;
+    for(int j=1;j<=i;j++)
+    {
+      cout<<c<<" ";
+      c=c*(i-j)/j;
+    }
+    cout<<endl;
+  }
+}
+int main()
+{
+  int a;
+  cout <<"Enter the number of the rows:";
+  cin>>a;
+  pattern(a);
+  return 0;
+}
